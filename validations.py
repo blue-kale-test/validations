d93e8782fbe1ff9999eaf2a8cb6652bb74af3502
ski123786 <isudeshk@gmail.com>
2021-07-07 17:27:07 -0500

    Update validations.py

--- validations.py
+++ validations.py
@@ -6,7 +6,7 @@ def validate_user(username, minlen):
   if type(username) != str:
     raise TypeError("username must be a string")
   if minlen < 1:
-    raise ValueError("minlen must be at least 1")
+    raise ValueError("min len must be at least 1")
     
   if len(username) < minlen:
       return False
