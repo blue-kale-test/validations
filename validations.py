e1540af871cee44b2c7959309217fa45ffc4c444
Nilesh1978 <Nilesh1978@users.noreply.github.com>
2021-10-03 15:55:32 -0500

    added author name to validations.py file

--- validations.py
+++ validations.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-
+# Author: Blue Kale
 def validate_user(username, minlen):
   """Checks if the received username matches the required conditions."""
   if type(username) != str:
