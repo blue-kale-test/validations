156ecbd26faf0010a0548676a715518dd74208a6
Rey Okudzeto <63571185+rey-okujet@users.noreply.github.com>
2022-08-13 08:50:10 -0600

    Just practising how to do a pull request

--- validations.py
+++ validations.py
@@ -3,6 +3,7 @@
 
 def validate_user(username, minlen):
   """Checks if the received username matches the required conditions."""
+  """Just added a dummy comment in order to do this pull request"""
   if type(username) != str:
     raise TypeError("username must be a string")
   if minlen < 1:
