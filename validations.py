2ea5e03349ef24627c19446e2ec124c9a7ab2a80
Ismet ERDOGAN <76628845+ismeterd@users.noreply.github.com>
2022-08-22 15:58:25 +0300

    Added a new comment line (not important).

--- validations.py
+++ validations.py
@@ -3,6 +3,7 @@
 
 def validate_user(username, minlen):
   """Checks if the received username matches the required conditions."""
+  """to send pull request"""
   if type(username) != str:
     raise TypeError("username must be a string")
   if minlen < 1:
