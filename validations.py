d94145852f29416146b4c90b7c7d9b7827088f7e
Yogiraj Dange <46704347+yogirajdange@users.noreply.github.com>
2023-04-01 12:45:14 +0530

    fixed typo in function documentation

--- validations.py
+++ validations.py
@@ -2,11 +2,11 @@
 
 
 def validate_user(username, minlen):
-  """Checks if the received username matches the required conditions."""
+  """Checks if the recived username matches the required conditions."""
   if type(username) != str:
     raise TypeError("username must be a string")
   if minlen < 1:
-    raise ValueError("minlen must be at least 1")
+    raise Value-Error("minlen must be at least 1")
     
   if len(username) < minlen:
       return False
